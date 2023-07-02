ae904eb18dc39546f8768015eee10c220a09758a
re045908 <maikel.souza@qintess.com>
2023-07-02 12:15:19 -0300

    created more artefacts

--- src/main/java/Config/ConnectionFactory.java
+++ src/main/java/Config/ConnectionFactory.java
@@ -8,15 +8,13 @@ public class ConnectionFactory {
 
     public static Connection createConnection() throws SQLException {
 
-//        String url = "jdbc:mysql://localhost:3306/condvirtuais2"; //Nome da base de dados
-//        String user = "root"; //nome do usuário do MySQL
-//        String password = "root"; //senha do MySQL
+        String url = "jdbc:mysql://localhost:3306/condvirtuais2"; //Nome da base de dados
+        String user = "root"; //nome do usuário do MySQL
+        String password = "root"; //senha do MySQL
 
 
 
-        String url = "jdbc:oracle:thin:@//scan-sdfdevxv.pf.gov.br:1521/dpf06ta"; //Nome da base de dados
-        String user = "AGESPUSR"; //nome do usuário do MySQL
-        String password = "AGESPUSR"; //senha do MySQL
+
         // TODO: VERIFCAR ONDE CONFIGURAR O SCHEMA
 
         return DriverManager.getConnection(url, user, password);
